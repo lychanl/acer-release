79fd0c0673fad0a9a27fe532494d144c23b7c223
Marcin Szulc <m.szulc2@protonmail.com>
2020-04-13 11:12:15 +0200

    Move RunningMeanVarianceTf to tf_utils.py

--- acer/tf_utils.py
+++ acer/tf_utils.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Tuple
 
 import numpy as np
 import tensorflow as tf
@@ -18,3 +18,44 @@ def kronecker_prod(x: Union[tf.Tensor, np.array], y: Union[tf.Tensor, np.array])
     operator_2 = tf.linalg.LinearOperatorFullMatrix(y)
     prod = tf.linalg.LinearOperatorKronecker([operator_1, operator_2]).to_dense()
     return prod
+
+
+class RunningMeanVarianceTf:
+    def __init__(self, epsilon: float = 1e-4, shape: Tuple = ()):
+        """TensorFlow version of RunningMeanVariance
+
+        Args:
+            epsilon: small value for numerical stability
+            shape: shape of the normalized vector
+        """
+        self.mean = tf.zeros(shape=shape)
+        self.var = tf.ones(shape=shape)
+        self.count = tf.Variable(initial_value=epsilon)
+
+    def update(self, x: tf.Tensor):
+        """Updates statistics with given batch [batch_size, vector_size] of samples
+
+        Args:
+            x: batch of samples
+        """
+        batch_mean = tf.reduce_mean(x, axis=0)
+        batch_var = tf.math.reduce_variance(x, axis=0)
+        batch_count = x.shape[0]
+
+        if tf.math.less(self.count, 1):
+            self._assign_new_values(batch_count, batch_mean, batch_var)
+        else:
+            new_count = self.count + batch_count
+            delta = batch_mean - self.mean
+            new_mean = self.mean + delta * batch_count / new_count
+
+            m_a = self.var * (self.count - tf.constant(1))
+            m_b = batch_var * (batch_count - tf.constant(1))
+            m_2 = m_a + m_b + np.square(delta) * self.count * batch_count / new_count
+            new_var = m_2 / (new_count - tf.constant(1))
+            self._assign_new_values(new_count, new_mean, new_var)
+
+    def _assign_new_values(self, count: tf.Tensor, mean: tf.Tensor, var: tf.Tensor):
+        self.count.assign(count)
+        self.mean.assign(mean)
+        self.var.assign(var)
--- acer/utils.py
+++ acer/utils.py
@@ -115,47 +115,6 @@ class RunningMeanVariance:
             pickle.dump(self, f)
 
 
-class RunningMeanVarianceTf:
-    def __init__(self, epsilon: float = 1e-4, shape: Tuple = ()):
-        """TensorFlow version of RunningMeanVariance
-
-        Args:
-            epsilon: small value for numerical stability
-            shape: shape of the normalized vector
-        """
-        self.mean = tf.zeros(shape=shape)
-        self.var = tf.ones(shape=shape)
-        self.count = tf.Variable(initial_value=epsilon)
-
-    def update(self, x: tf.Tensor):
-        """Updates statistics with given batch [batch_size, vector_size] of samples
-
-        Args:
-            x: batch of samples
-        """
-        batch_mean = tf.reduce_mean(x, axis=0)
-        batch_var = tf.math.reduce_variance(x, axis=0)
-        batch_count = x.shape[0]
-
-        if tf.math.less(self.count, 1):
-            self._assign_new_values(batch_count, batch_mean, batch_var)
-        else:
-            new_count = self.count + batch_count
-            delta = batch_mean - self.mean
-            new_mean = self.mean + delta * batch_count / new_count
-
-            m_a = self.var * (self.count - tf.constant(1))
-            m_b = batch_var * (batch_count - tf.constant(1))
-            m_2 = m_a + m_b + np.square(delta) * self.count * batch_count / new_count
-            new_var = m_2 / (new_count - tf.constant(1))
-            self._assign_new_values(new_count, new_mean, new_var)
-
-    def _assign_new_values(self, count: tf.Tensor, mean: tf.Tensor, var: tf.Tensor):
-        self.count.assign(count)
-        self.mean.assign(mean)
-        self.var.assign(var)
-
-
 def timing(f):
     """Function decorator that measures time elapsed while executing a function."""
     @wraps(f)
