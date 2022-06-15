actors fix, clip weighted param in TD2Reg, variance funs

acer/algos/actors.py
@@ -41,7 +41,7 @@ class StdClippedBaseActor:
         if self._scale_td:
             clipped = clipped / std
 
-        return tf.stop_gradient(weighted)
+        return tf.stop_gradient(clipped)
 
 
 class StdClippedCategoricalActor(CategoricalActor, StdClippedBaseActor):
@@ -105,10 +105,11 @@ class TD2RegularizedGaussianActor(GaussianActor, TD2RegularizedActor):
         TD2RegularizedActor.__init__(self, *args, **kwargs)
 
 class TD2RegStdClippedBaseActor(TD2RegularizedActor):
-    def __init__(self, *args, alpha=1, eps=0, eta=0.1, kappa=1, scale2_td2, **kwargs) -> None:
+    def __init__(self, *args, alpha=1, eps=0, eta=0.1, kappa=1, scale2_td2=False, clip_weighted=False, **kwargs) -> None:
         self._alpha = alpha
         self._eps = eps
         self._scale2_td2 = scale2_td2
+        self._clip_weighted = clip_weighted
         super().__init__(*args, eta=eta, kappa=kappa, **kwargs)
 
         self.register_method('optimize', self.optimize, {
@@ -129,8 +130,15 @@ class TD2RegStdClippedBaseActor(TD2RegularizedActor):
     def _calculate_clipped_regularized_weighted_td(self, td, weights, std, eta):
         std = tf.maximum(std, self._eps)[:,:,0]
         clip = self._alpha * std
-        clipped = tf.clip_by_value(td * weights, -clip, clip)
-        clipped2 = tf.clip_by_value(td ** 2 * weights, -clip ** 2, clip ** 2)
+
+        if self._clip_weighted:
+            td = td * weights
+            td2 = td ** 2 * weights
+        else:
+            td2 = td ** 2
+
+        clipped = tf.clip_by_value(td, -clip, clip)
+        clipped2 = tf.clip_by_value(td2, -clip ** 2, clip ** 2)
 
         clipped = clipped / std
 
@@ -139,7 +147,12 @@ class TD2RegStdClippedBaseActor(TD2RegularizedActor):
         else:
             clipped2 = clipped2 / std
 
-        return tf.stop_gradient(clipped - eta * clipped2)
+        regularized = clipped - eta * clipped2
+
+        if self._clip_weighted:
+            return tf.stop_gradient(regularized)
+        else:
+            return tf.stop_gradient(regularized * weights)
 
 
 class TD2RegStdClippedCategoricalActor(CategoricalActor, TD2RegStdClippedBaseActor):

acer/algos/critics.py
@@ -5,14 +5,22 @@ import numpy as np
 import tensorflow as tf
 
 
+VARIANCE_FUNS = {
+    'identity': tf.identity,
+    'exp': tf.exp
+}
+
+
 class VarianceCritic(AutoModelComponent):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args, variance_fun='identity', **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self._value_critic = Critic(*args, **kwargs)
         self._value2_critic = Critic(*args, **kwargs)
 
         self.value = self._value_critic.value
         self.value2 = self._value2_critic.value
+
+        self.variance_fun = VARIANCE_FUNS[variance_fun]
 
         self.register_method('value', self.value, {'observations': 'obs'})
         self.register_method('value_next', self.value, {'observations': 'obs_next'})
@@ -41,12 +49,12 @@ class VarianceCritic(AutoModelComponent):
 
     @tf.function
     def std(self, value2):
-        variance = value2
+        variance = self.variance_fun(value2)
         return tf.sqrt(tf.maximum(variance, 0))
 
     @tf.function
     def _calculate_td2(self, td, values2):
-        td2 = td ** 2 - values2[:,:1]
+        td2 = td ** 2 - self.variance_fun(values2[:,:1])
 
         return td2
 
@@ -55,18 +63,18 @@ class VarianceCritic(AutoModelComponent):
         return tf.stop_gradient(td2 * weights)
 
     @tf.function
-    def loss(self, critic, obs, d) -> tf.Tensor:
-        value = critic.value(obs)
+    def loss(self, critic, fun, obs, d) -> tf.Tensor:
+        value = fun(critic.value(obs))
         return tf.reduce_mean(-tf.math.multiply(value, d))
 
     @tf.function
     def optimize(self, obs, d, d2):
-        for critic, cd, in [
-            (self._value_critic, d),
-            (self._value2_critic, d2)
+        for critic, fun, cd, in [
+            (self._value_critic, tf.identity, d),
+            (self._value2_critic, self.variance_fun, d2)
         ]:
             with tf.GradientTape() as tape:
-                loss_v = self.loss(critic, obs, cd)
+                loss_v = self.loss(critic, fun, obs, cd)
             grads = tape.gradient(loss_v, critic.trainable_variables)
             gradients = zip(grads, critic.trainable_variables)
 

acer/algos/fast_acer.py
@@ -11,6 +11,7 @@ Neural Networks : the Official Journal of the International Neural Network Socie
 Wawrzyński, Paweł. "Real-time reinforcement learning by sequential actor–critics
 and experience replay." Neural Networks 22.10 (2009): 1484-1497.
 """
+from tabnanny import check
 from typing import Optional, List, Union, Dict, Tuple
 import gym
 import tensorflow as tf
@@ -77,7 +78,8 @@ class FastACER(BaseACERAgent):
     def __init__(self, observations_space: gym.Space, actions_space: gym.Space, actor_layers: Optional[Tuple[int]],
                  critic_layers: Optional[Tuple[int]], b: float = 3, no_truncate: bool = True,
                  update_blocks=1, buffer_type='simple', log_values=(), log_memory_values=(),
-                 actor_type='simple', critic_type='simple', nan_guard=False, *args, **kwargs):
+                 actor_type='simple', critic_type='simple', nan_guard=False, log_to_file_values=(),
+                 *args, **kwargs):
         """BaseActor-Critic with Experience Replay
 
         TODO: finish docstrings
@@ -106,6 +108,8 @@ class FastACER(BaseACERAgent):
         self._b = b
         self._truncate = not no_truncate
 
+        super().__init__(observations_space, actions_space, actor_layers, critic_layers, *args, **kwargs)
+
         self.PREPROCESSING = {
             'obs': self._process_observations,
             'obs_next': self._process_observations,
@@ -127,13 +131,22 @@ class FastACER(BaseACERAgent):
                 target_list.append((log_value, val, gather))
             return target_list
 
+        def check_log_values(log_values, call_list_data, call_list, where=""):
+            for _, value, _ in log_values:
+                assert value in call_list_data or value in [n for n, _, _ in call_list],\
+                    f'Error: {value} not calculated {where}'
+
+
         self._log_values = prepare_log_values(log_values)
         self._log_memory_values = prepare_log_values(log_memory_values)
+        self._log_to_file_values = prepare_log_values(log_to_file_values)
         self._nan_guard = nan_guard
         self._nan_log_prev_mem_batch = None
         self._nan_log_prev_batch = None
 
-        super().__init__(observations_space, actions_space, actor_layers, critic_layers, *args, **kwargs)
+        check_log_values(self._log_values, self._call_list_data, self._call_list)
+        check_log_values(self._log_to_file_values, self._call_list_data, self._call_list)
+        check_log_values(self._log_memory_values, self._memory_call_list_data, self._memory_call_list, "in memory updates")
 
     def _init_automodel(self, skip=()):
         self.register_method("mask", self._calculate_mask, {"lengths": "lengths", "n": "memory_params.n"})
@@ -173,6 +186,8 @@ class FastACER(BaseACERAgent):
             replay_gen, dtypes = self._get_experience_replay_generator(seq=True, fields=self._memory_call_list_data)
             self._buffer_update_loader = tf.data.Dataset.from_generator(
                 replay_gen, dtypes).prefetch(1)
+        else:
+            self._memory_call_list = self._memory_call_list_data = None
 
     def _init_automodel_overrides(self) -> None:
         pass
@@ -241,9 +256,11 @@ class FastACER(BaseACERAgent):
 
             experience_replay_iterations = min([round(self._c0 * self._time_step), self._c])
             
+            outs = []
             for batch in self._data_loader.take(experience_replay_iterations):
                 data = {f: d for f, d in zip(self._call_list_data, batch)}
-                self._learn_from_experience_batch(data)
+                out = self._learn_from_experience_batch(data)
+                outs.append(out)
                 if self._nan_guard:
                     if not np.isfinite(self._actor.act_deterministic(data['obs']).numpy()).all():
                         print('NaN on learn step')
@@ -252,6 +269,7 @@ class FastACER(BaseACERAgent):
                         print('Last batch:')
                         print_batch(self._nan_log_prev_batch)
                     self._nan_log_prev_batch = data
+            return np.mean(outs, 0)
     
     @tf.function(experimental_relax_shapes=True)
     def _calculate_memory_update(self, data):
@@ -270,6 +288,8 @@ class FastACER(BaseACERAgent):
         with tf.name_scope('log'):
             for name, value, gather in self._log_values:
                 tf.summary.scalar(name, gather(data[value]), self._tf_time_step)
+        return [gather(data[value]) for _, value, gather in self._log_to_file_values]
+
 
 
     @tf.function(experimental_relax_shapes=True)

acer/runners.py
@@ -73,7 +73,7 @@ class Runner:
                  num_evaluation_runs: int = 5, log_dir: str = 'logs/', max_time_steps: int = -1,
                  record_end: bool = True, experiment_name: str = None, asynchronous: bool = True,
                  log_tensorboard: bool = True, do_checkpoint: bool = True, record_time_steps: int = None,
-                 periodic_log: int = -1, dump=()):
+                 periodic_log: int = -1, dump=(), log_to_file_values=(), log_to_file_steps=1000):
         """Trains and evaluates the agent.
 
         Args:
@@ -96,6 +96,7 @@ class Runner:
         self._done_episodes = 0
         self._next_evaluation_timestamp = 0
         self._next_record_timestamp = 0
+        self._next_log_values_timestamp = log_to_file_steps  # don't log at the beginning
         self._n_envs = num_parallel_envs
         self._evaluate_time_steps_interval = evaluate_time_steps_interval
         self._n_step = n_step
@@ -104,6 +105,9 @@ class Runner:
         self._log_tensorboard = log_tensorboard
         self._do_checkpoint = do_checkpoint
         self._env_name = environment_name
+
+        self._log_to_file_values = log_to_file_values
+        self._log_to_file_steps = log_to_file_steps
 
         self._record_end = record_end
         self._record_time_steps = record_time_steps
@@ -130,6 +134,10 @@ class Runner:
             self._log_dir / 'results.csv',
             keys=['time_step', 'eval_return_mean', 'eval_std_mean']
         )
+        if self._log_to_file_values:
+            self._values_csv_logger = CSVLogger(self._log_dir / 'values.csv', keys=['time_step'] + list(self._log_to_file_values))
+        else:
+            self._values_csv_logger = None
         if periodic_log > 0:
             self._logger = PeriodicConsoleLogger(periodic_log)
         else:
@@ -168,6 +176,7 @@ class Runner:
         uses policy that is being optimized, not the one used in training (i.e. randomness is turned off)
         """
         try:
+            logged_values = []
             while self._max_time_steps == -1 or self._time_step <= self._max_time_steps:
                 if self._is_time_to_evaluate():
                     self._evaluate()
@@ -175,6 +184,10 @@ class Runner:
                         self._save_results()
                         if self._do_checkpoint:
                             self._save_checkpoint()
+                
+                if self._is_time_to_log_values():
+                    self._log_values(logged_values)
+                    logged_values = []
 
                 if self._is_time_to_record():
                     self.record_video()
@@ -183,7 +196,9 @@ class Runner:
                 experience = self._step()
                 self._agent.save_experience(experience)
                 if self._time_step % self._n_step == 0:
-                    self._agent.learn()
+                    values = self._agent.learn()
+                    if values is not None:
+                        logged_values.append(values)
                 self._elapsed_time_measure += time.time() - start_time
 
                 if self._time_step in self._dump:
@@ -192,6 +207,8 @@ class Runner:
 
             self._logger.flush(self._time_step - 1)
             self._csv_logger.close()
+            if self._values_csv_logger:
+                self._values_csv_logger.close()
             if self._record_end:
                 self.record_video()
         except:
@@ -326,6 +343,23 @@ class Runner:
                 self._time_step
             )
         self._elapsed_time_measure = 0
+
+    def _is_time_to_log_values(self):
+        return self._log_to_file_values and self._time_step >= self._next_log_values_timestamp
+
+    def _log_values(self, values):
+        self._next_log_values_timestamp += self._log_to_file_steps
+
+        if not values:
+            return
+
+        means = np.mean(values, 0)
+        means_dict = {name: value for name, value in zip(self._log_to_file_values, means)}
+        means_dict['time_step'] = self._time_step
+
+        self._values_csv_logger.log_values(means_dict)
+        self._values_csv_logger.dump()
+        self._logger.log_values(means_dict)
 
     def _save_parameters(self, algorithm_parameters: dict):
         with open(str(self._log_dir / 'parameters.json'), 'wt') as f: